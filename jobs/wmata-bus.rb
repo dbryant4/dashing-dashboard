cf63122b27d04ae9fc3baf58873ec5717854a452
Derrick Bryant <dbryant@millennialmedia.com>
2013-12-04 13:13:59 -0500

    Moved wmata information to wmata-config.yaml

--- jobs/wmata-bus.rb
+++ jobs/wmata-bus.rb
@@ -1,12 +1,15 @@
 require 'yaml'
 
+wmata_config = YAML.load_file('wmata-config.yaml')
+
 # This script connects to WMATA's API. Therefore, you need a WMATA api key.
-api_keys = YAML.load_file('wmata-config.yaml')['wmata-api-keys']
+api_keys = wmata_config['wmata-api-keys']
 api_uri = 'http://api.wmata.com/NextBusService.svc/json/jPredictions'
-stop_ids = [1002873, 1001947, 1003040, 1003728]
+config_defaults = wmata_config['bus']['defaults']
+stop_ids = wmata_config['bus']['stops']
 
 SCHEDULER.every '30s', :first_in => 0  do
-  stop_ids.each do |stop_id|
+  stop_ids.each do |stop_id, options|
     api_key = api_keys.sample
     uri = URI("#{api_uri}?StopID=#{stop_id}&api_key=#{api_key}")
     req = Net::HTTP::Get.new(uri.request_uri)
@@ -14,12 +17,24 @@ SCHEDULER.every '30s', :first_in => 0  do
       http.request(req)
     }
 
+    if options != nil and options['prediction_limit']
+      prediction_limit = options['prediction_limit']
+    else
+      prediction_limit = wmata_config['bus']['defaults']['prediction_limit']
+    end
+
+    if options != nil and options['char_limit']
+      char_limit = options['char_limit'] - 1
+    else
+      char_limit = wmata_config['bus']['defaults']['char_limit'] - 1
+    end
+
     predictions = JSON.parse(res.body)['Predictions']
     processed_predictions = Array.new()
     num_buses = 0
     predictions.each do |prediction|
-      if num_buses < 7 then
-        processed_predictions.push( { label: "#{prediction['RouteID']} #{prediction['DirectionText'][0..23]}", value: "#{prediction['Minutes']}"} )
+      if num_buses < prediction_limit
+        processed_predictions.push( { label: "#{prediction['RouteID']} #{prediction['DirectionText'][0..char_limit]}", value: "#{prediction['Minutes']}"} )
         num_buses += 1
       end
     end
--- jobs/wmata-rail.rb
+++ jobs/wmata-rail.rb
@@ -1,18 +1,29 @@
 require 'yaml'
 
+
+wmata_config = YAML.load_file('wmata-config.yaml')
+
 # This script connects to WMATA's API. Therefore, you need a WMATA api key.
-api_keys = YAML.load_file('wmata-config.yaml')['wmata-api-keys']
+api_keys = wmata_config['wmata-api-keys']
 api_uri = 'http://api.wmata.com/StationPrediction.svc/json/GetPrediction/'
-station_mappings = {
-  'galleryplace' => 'B01,F01',
-  'metrocenter' => 'A01,C01',
-  'columbiaheights' => 'E04',
-  'woodleypark' => 'A04'
-}
 
 SCHEDULER.every '30s', :first_in => 0  do
-  station_mappings.each do |station, codes|
+  wmata_config['rail']['stations'].each do |station, options|
     api_key = api_keys.sample
+    codes = options['codes'].join(',')
+
+    if options['prediction_limit']
+      prediction_limit = options['prediction_limit']
+    else
+      prediction_limit = wmata_config['rail']['defaults']['prediction_limit']
+    end
+
+    if options['char_limit']
+      char_limit = options['char_limit']
+    else
+      char_limit = wmata_config['rail']['defaults']['char_limit']
+    end
+
     uri = URI("#{api_uri}#{codes}?api_key=#{api_key}")
     req = Net::HTTP::Get.new(uri.request_uri)
     res = Net::HTTP.start(uri.hostname, uri.port) {|http|
@@ -20,11 +31,11 @@ SCHEDULER.every '30s', :first_in => 0  do
     }
 
     next_trains = JSON.parse(res.body)['Trains']
-    trains = Array.new(7)
+    trains = Array.new()
     num_trains = 0
     next_trains.each do |train|
-      if num_trains < 7 then
-        trains.push( { label: "#{train['Line']} #{train['DestinationName']}", value: "#{train['Min']}"} )
+      if num_trains < prediction_limit then
+        trains.push( { label: "#{train['Line']} #{train['DestinationName'][0..char_limit]}", value: "#{train['Min']}"} )
         num_trains += 1
       end
     end
