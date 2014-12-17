5358bba8a133a6b040363b3958fd97aba58944f9
Derrick Bryant <dbryant4@gmail.com>
2014-12-16 22:35:20 -0500

    Added surge pricing to uber

--- jobs/uber.rb
+++ jobs/uber.rb
@@ -7,6 +7,7 @@ uber_config = YAML.load_file('uber-config.yaml')
 server_token = uber_config['server_token']
 time_estimates_api_uri = "https://api.uber.com/v1/estimates/time"
 products_api_uri = "https://api.uber.com/v1/products"
+price_estimates_api_uri = "https://api.uber.com/v1/estimates/price"
 
 SCHEDULER.every '30s', :first_in => 0  do
   uber_config['points_of_interest'].each do |poi, options|
@@ -23,6 +24,21 @@ SCHEDULER.every '30s', :first_in => 0  do
       images[product['product_id']] = product['image']
     end
 
+    uri = URI("#{price_estimates_api_uri}?start_latitude=#{options['latitude']}&start_longitude=#{options['longitude']}&end_latitude=#{options['latitude']}&end_longitude=#{options['longitude']}")
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['Authorization'] = "Token #{server_token}"
+    res = http.request(request)
+
+    surge_multipliers = {}
+    prices = JSON.parse(res.body)['prices']
+    prices.each do |price|
+      surge_multipliers[price['product_id']] = price['surge_multiplier']
+    end
+
     uri = URI("#{time_estimates_api_uri}?start_latitude=#{options['latitude']}&start_longitude=#{options['longitude']}")
     http = Net::HTTP.new(uri.hostname, uri.port)
     http.use_ssl = true
@@ -37,7 +53,9 @@ SCHEDULER.every '30s', :first_in => 0  do
       time['estimate'] = time['estimate'].to_i / 60
       time['estimate'] = time['estimate'].round(0)
       time['image'] = images[time['product_id']]
+      time['surge_multiplier'] = surge_multipliers[time['product_id']]
     end
+
     send_event("uber-times-#{poi}", { :times => times, :poi_name => poi, :origin => [options['latitude'], options['longitude']] })
   end
 
